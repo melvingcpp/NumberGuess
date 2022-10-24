ccc21c4f128b4c09855b9faec9f41dcfbf839376
Melvin <melvingit@gmail.com>
2022-10-23 21:16:30 -0700

    created outline

---
+++ NumberGuess.c
@@ -0,0 +1,7 @@
+//prompt user to input a number between 1-3
+
+//if 1 is selected, prompt user to enter a number to guess
+
+//if 2 is selected, promt user to enter a value
+
+//if 3 is selected, end program
